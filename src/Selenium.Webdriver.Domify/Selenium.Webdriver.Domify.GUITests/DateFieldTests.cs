0afc0c1d039c2b573ed3925617100655d014f1a5
Yngve Bakken Nilsen <yngve.nilsen@gmail.com>
2013-03-18 15:20:10 +0100

    Tests
    #2

--- src/Selenium.Webdriver.Domify/Selenium.Webdriver.Domify.GUITests/DateFieldTests.cs
+++ src/Selenium.Webdriver.Domify/Selenium.Webdriver.Domify.GUITests/DateFieldTests.cs
@@ -33,32 +33,4 @@ namespace Selenium.Webdriver.Domify.GUITests
             Document.Navigation.GetCurrentPage<HomeIndex>().DateBox.SetDate(_textToTypeIntoTextBox);
         }
     }
-
-    [TestFixture]
-    public class NavigationTests : BrowserScenario
-    {
-
-        [SetUp]
-        public void SetupBrowser()
-        {
-
-        }
-
-        [Then]
-        public void WeShouldBeAtHomePage()
-        {
-            Assert.That(Document.Navigation.IsAtPage<HomeIndex>());
-            Assert.That(Document.Navigation.GetCurrentPage<HomeIndex>(), Is.Not.Null);
-        }
-
-        protected override void Given()
-        {
-            Document.Navigation.GoTo<HomeIndex>();
-        }
-
-        protected override void When()
-        {
-
-        }
-    }
 }
