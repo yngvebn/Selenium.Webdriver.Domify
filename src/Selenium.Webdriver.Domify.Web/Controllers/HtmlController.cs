f1f3f11758bc24ad53ecbc319bc0c0e050a8e8ed
Yngve Bakken Nilsen <yngve.nilsen@outlook.com>
2013-09-05 19:35:01 +0200

    Added page for formelement-testing
    Tests not yet implemented

---
+++ src/Selenium.Webdriver.Domify.Web/Controllers/HtmlController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Selenium.Webdriver.Domify.Web.Controllers
+{
+    public class HtmlController: Controller
+    {
+         public ActionResult Forms()
+         {
+             return View();
+         }
+    }
+}
