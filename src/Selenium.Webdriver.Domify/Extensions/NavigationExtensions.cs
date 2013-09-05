278f83b4f9a1b87eac8f9e985f0d52479d36a90a
Yngve Bakken Nilsen <yngve.nilsen@outlook.com>
2013-09-05 20:58:30 +0200

    Minor changes

--- src/Selenium.Webdriver.Domify/Extensions/NavigationExtensions.cs
+++ src/Selenium.Webdriver.Domify/Extensions/NavigationExtensions.cs
@@ -6,15 +6,15 @@ namespace Selenium.Webdriver.Domify
 {
     public static class NavigationExtensions
     {
-        public static T Page<T>(this INavigationService webDriver) where T : Page, new()
+        private static T Page<T>(this INavigationService webDriver) where T : Page, new()
         {
-            var t = new T { Document = webDriver.Document };
+            var t = new T {Document = webDriver.Document};
             return t;
         }
-        
+
 
         /// <summary>
-        /// Shortcut to Current.Page
+        ///     Shortcut to Current.Page
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
@@ -24,18 +24,19 @@ namespace Selenium.Webdriver.Domify
         }
 
         /// <summary>
-        /// Browses to the given Page with the current browser window
+        ///     Browses to the given Page with the current browser window
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
         public static T GoTo<T>(this INavigationService document) where T : Page, new()
         {
-            var navigationInfo = TryGetPageDescriptionAttribute<T>();
+            PageDescriptionAttribute navigationInfo = TryGetPageDescriptionAttribute<T>();
 
             if (navigationInfo != null)
                 document.GoToPageUrl(navigationInfo.Url);
             else
-                throw new InvalidOperationException("You are trying to navigate to a page which does not specify its uri (missing PageDescriptionAttribute)");
+                throw new InvalidOperationException(
+                    "You are trying to navigate to a page which does not specify its uri (missing PageDescriptionAttribute)");
 
             return document.GetCurrentPage<T>();
         }
@@ -47,33 +48,32 @@ namespace Selenium.Webdriver.Domify
 
 
         /// <summary>
-        /// Browses to the given URI with the current browser window
+        ///     Browses to the given URI with the current browser window
         /// </summary>
         public static void GoTo(this INavigationService document, Uri uri)
         {
             document.GoToPageUrl(uri);
-
         }
 
         /// <summary>
-        /// Browses to the given Page with the current browser window
+        ///     Browses to the given Page with the current browser window
         /// </summary>
         public static object GoTo(this INavigationService document, Type t)
         {
-            var navigationInfo = TryGetPageDescriptionAttribute(t);
+            PageDescriptionAttribute navigationInfo = TryGetPageDescriptionAttribute(t);
 
             if (navigationInfo != null)
                 document.GoToPageUrl(navigationInfo.Url);
             else
-                throw new InvalidOperationException("You are trying to navigate to a page which does not specify its uri (missing PageDescriptionAttribute)");
-            MethodInfo method = typeof(NavigationExtensions).GetMethod("GetCurrentPage");
+                throw new InvalidOperationException(
+                    "You are trying to navigate to a page which does not specify its uri (missing PageDescriptionAttribute)");
+            MethodInfo method = typeof (NavigationExtensions).GetMethod("GetCurrentPage");
             MethodInfo genericMethod = method.MakeGenericMethod(t);
             return genericMethod.Invoke(null, null);
-
         }
 
         /// <summary>
-        /// Navigates to the given page with the additional URL-string
+        ///     Navigates to the given page with the additional URL-string
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="document"></param>
@@ -81,61 +81,63 @@ namespace Selenium.Webdriver.Domify
         /// <returns></returns>
         public static T GoTo<T>(this INavigationService document, string appendToUrl) where T : Page, new()
         {
-            var navigationInfo = TryGetPageDescriptionAttribute<T>();
+            PageDescriptionAttribute navigationInfo = TryGetPageDescriptionAttribute<T>();
 
             if (navigationInfo != null)
                 document.GoToPageUrl(Build(navigationInfo.Url, appendToUrl));
             else
-                throw new InvalidOperationException("You are trying to navigate to a page which does not specify its uri (missing PageDescriptionAttribute)");
+                throw new InvalidOperationException(
+                    "You are trying to navigate to a page which does not specify its uri (missing PageDescriptionAttribute)");
 
             return document.GetCurrentPage<T>();
         }
 
         private static Uri Build(Uri url, string appendToUrl)
         {
-            Uri uri = new Uri(url + appendToUrl);
+            var uri = new Uri(url + appendToUrl);
             return uri;
         }
 
         /// <summary>
-        /// Checks if the current browser is at the given page
+        ///     Checks if the current browser is at the given page
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="document"></param>
         /// <returns></returns>
         public static bool IsAtPage<T>(this INavigationService document) where T : Page, new()
         {
-            return document.IsAtPage(typeof(T));
+            return document.IsAtPage(typeof (T));
         }
 
         /// <summary>
-        /// Checks if the current browser is at the given page
+        ///     Checks if the current browser is at the given page
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="document"></param>
         /// <returns></returns>
         public static bool IsAtPage(this INavigationService document, Type pageType)
         {
-            var navigationInfo = TryGetPageDescriptionAttribute(pageType);
+            PageDescriptionAttribute navigationInfo = TryGetPageDescriptionAttribute(pageType);
 
             if (navigationInfo == null)
                 throw new InvalidOperationException("The page type does not specify its uri");
 
-            return String.Equals(navigationInfo.Url.ToString(), document.Document.Uri.AbsolutePath, StringComparison.InvariantCultureIgnoreCase) ||
-                   String.Equals(navigationInfo.Url.ToString(), document.Document.Uri.ToString(), StringComparison.InvariantCultureIgnoreCase);
+            return
+                String.Equals(navigationInfo.Url.ToString(), document.Document.Uri.AbsolutePath,
+                              StringComparison.InvariantCultureIgnoreCase) ||
+                String.Equals(navigationInfo.Url.ToString(), document.Document.Uri.ToString(),
+                              StringComparison.InvariantCultureIgnoreCase);
         }
 
 
-      private static PageDescriptionAttribute TryGetPageDescriptionAttribute(Type t)
-      {
-          return CacheHolder.TryGetPageDescriptionAttribute(t);
-      }
+        private static PageDescriptionAttribute TryGetPageDescriptionAttribute(Type t)
+        {
+            return CacheHolder.TryGetPageDescriptionAttribute(t);
+        }
 
         private static PageDescriptionAttribute TryGetPageDescriptionAttribute<T>()
         {
-            return CacheHolder.TryGetPageDescriptionAttribute(typeof(T));
+            return CacheHolder.TryGetPageDescriptionAttribute(typeof (T));
         }
-
-
     }
 }
