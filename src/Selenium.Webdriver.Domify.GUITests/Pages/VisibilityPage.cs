6bc81f8a3349ef8b1c0968d70c415d71d457f3c1
unknown <yngve.nilsen@gmail.com>
2014-08-25 13:11:59 +0200

    Fixed findbyText

---
+++ src/Selenium.Webdriver.Domify.GUITests/Pages/VisibilityPage.cs
@@ -0,0 +1,8 @@
+namespace Selenium.Webdriver.Domify.GUITests.Pages
+{
+    [PageDescription("Visibility", "http://localhost:31338/Home/Visibility")]
+    public class VisibilityPage : Page
+    {
+        
+    }
+}
---
+++ src4.5.1/Selenium.Webdriver.Domify.GUITests/Tests/ElementStyles/VisibilityTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Selenium.Webdriver.Domify.Elements;
+using Selenium.Webdriver.Domify.GUITests.Core;
+using Selenium.Webdriver.Domify.GUITests.Pages;
+
+namespace Selenium.Webdriver.Domify.GUITests.Tests.ElementStyles
+{
+    public class VisibilityTests: PageScenario<VisibilityPage>
+    {
+        protected override void Given()
+        {
+            Document.Settings.BaseUri = new Uri("http://localhost:31338");
+            base.Given();
+        }
+
+        protected override void When()
+        {
+        }
+
+        [Then]
+        public void WeShouldFindOneVisibleButton()
+        {
+            var buttons = CurrentPage.Document.Find<Button>(Find.ByText("Button")).Where(d => d.IsVisible());
+            Assert.That(buttons.Count(), Is.EqualTo(1));
+        }
+
+        [Then]
+        public void WeShouldFindTwoButtonsTotal()
+        {
+            var buttons = CurrentPage.Document.Find<Button>(Find.ByText("Button"));
+            Assert.That(buttons.Count(), Is.EqualTo(2));
+        }
+    }
+}
