a37f550b33f5901045d42e9b1e1ac4fe04b8a93c
Magnus Hytten <magnus.hytten@gmail.com>
2015-03-31 14:01:01 +0200

    Added IsAtPage support for pages with parameters in url

---
+++ src/Selenium.Webdriver.Domify.Web/Controllers/IsAtPageController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace Selenium.Webdriver.Domify.Web.Controllers
+{
+    public class IsAtPageController : Controller
+    {
+        public ActionResult Index(int id)
+        {
+            return View(id);
+        }
+
+        public ActionResult ParametersInQuery(int queryValue)
+        {
+            return View(queryValue);
+        }
+    }
+}
